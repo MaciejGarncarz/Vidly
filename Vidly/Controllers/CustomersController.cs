3bf0b2aaee1cd7bfb81e81f9683b8b7cc7f882e4
Maciej Garncarz <maciej.garncarz@microsoft.wsei.edu.pl>
2018-07-21 12:40:23 +0200

    Addes navbar to partial view. Added customer page.

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModel;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ViewResult Index()
+        {
+            var movie = new Movie() { Name = "Poseidon" };
+            var customers = new List<Customer>
+            {
+                new Customer() {Name =  "Maciej", Surname = "Garncarz", Id = 1},
+                new Customer() {Name =  "Patrycja", Surname = "Biegun", Id = 2},
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+
+            return View(viewModel);
+        }
+
+        public ViewResult Detail(int id)
+        {
+            var customers = new List<Customer>
+            {
+                new Customer() {Name =  "Maciej", Surname = "Garncarz", Id = 1},
+                new Customer() {Name =  "Patrycja", Surname = "Biegun", Id = 2},
+            };
+
+            var customerToReturn = customers.SingleOrDefault(x => x.Id == id);
+
+            return View(customerToReturn);
+        }
+    }
+}
--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -4,16 +4,29 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModel;
 
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
         // GET: Movies
-        public ActionResult Random()
+        public ViewResult Random()
         {
             var movie = new Movie() {Name = "Poseidon"};
-            return View(movie);
+            var customers = new List<Customer>
+            {
+                new Customer() {Name =  "Maciej"},
+                new Customer() {Name =  "Patrycja"},
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
         }
     }
 }
